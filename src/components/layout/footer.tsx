6d22761c201a011d6e65716675776c0ecd07d18f
Imam Septian <imamseptian.wijaya@gmail.com>
2024-08-26 10:10:16 +0700

    refactor(ui): replace SVG icons with LucideIcon components
    Replaced several SVG icons with LucideIcon components to standardize icon usage and improve maintainability. This change allows for consistent styling and easier updates across the application.

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,5 +1,9 @@
 import { Button } from "@/components/ui/button";
-import { Globe, Linkedin } from "lucide-react";
+import {
+  Facebook,
+  Globe, Linkedin,
+  LucideGithub,
+} from "lucide-react";
 import Link from "next/link";
 
 export default function Footer() {
@@ -24,19 +28,7 @@ export default function Footer() {
                 rel="noopener noreferrer"
                 className="text-secondary-foreground hover:opacity-80"
               >
-                <svg
-                  className="w-4 h-4"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 8 19"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z"
-                    clipRule="evenodd"
-                  />
-                </svg>
+                <Facebook className="w-4 h-4" />
                 <span className="sr-only">Facebook page</span>
               </Link>
             </Button>
@@ -68,26 +60,14 @@ export default function Footer() {
                 rel="noopener noreferrer"
                 className="text-secondary-foreground hover:opacity-80 ms-5"
               >
-                <svg
-                  className="w-4 h-4"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
-                    clipRule="evenodd"
-                  />
-                </svg>
+                <LucideGithub className="w-4 h-4" />
                 <span className="sr-only">GitHub account</span>
               </Link>
             </Button>
 
             <Button variant="link" asChild>
               <Link
-                href="https://github.com/imamseptian"
+                href="https://www.linkedin.com/in/imam-septian"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-secondary-foreground hover:opacity-80 ms-5"
